8252d0de4f2d7a7ecc2f864015a093ecb9295600
Anil kumar <59431789+anilkumar541@users.noreply.github.com>
2022-10-04 12:59:29 +0530

    done with setup

--- grapes/settings.py
+++ grapes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^b1z)mgl%*%hmhq$8i@_af)^-dfsh_ctt^apfcygc8_eg=56c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -128,9 +128,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS=[BASE_DIR, "static"]
+
+MEDIA_URL="/media/"
+MEDIA_ROOT=BASE_DIR/'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ staticfiles/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn grapes.wsgi --log-file -
